3713754344296394a247ed98ae6b53b72749ab87
Michal Lurie <Michal.Lurie@gmail.com>
2025-09-07 00:55:12 +0200

    Update to 0.2.1

--- Casks/incplot.rb
+++ Casks/incplot.rb
@@ -2,11 +2,11 @@ cask "incplot" do
   arch arm: "arm64", intel: "x86_64"
   os macos: "macos", linux: "linux"
 
-  version "0.2.0"
-  sha256 arm:          "6c4d8c0981359fa0520b415c51ed687fb8d23dd8a04b3789d95f025af45f15e1",
-         x86_64:       "fe7ebb3dae17b3b46b9a5ef0d1a956721f21bf21b0bb672d36cea2fbb346a55d",
-         arm64_linux:  "d0818927f19d81a453ec2a84a6354efd8c5a05773dac63771b69d57b2fcdc9df",
-         x86_64_linux: "6cc6d3ad8bef9dc38ea3af0b44e58c42ed784ea9beffef9f0d4a45340d946c71"
+  version "0.2.1"
+  sha256 arm:          "b90ee6b37abfb6cbf2f921d5f98f6b2b031dfac7f2ff2fab602cf58d4380fd7b",
+         x86_64:       "b5bfb9fa9a1aef1908b2cee4bcd602ff19861484e8f949c5cceac1587f1fdc54",
+         arm64_linux:  "2d9ee0c3bd55afbb2caa434fb437864f397aee103866ee6993ed5d0e536291f4",
+         x86_64_linux: "62d6978c96bf321b19345a710384aae6b076da5f17331a5acd09b4d65853dc7a"
 
   url "https://github.com/InCom-0/incplot/releases/download/v#{version}/incplot_#{os}_#{arch}.tar.gz"
   name "incplot"
